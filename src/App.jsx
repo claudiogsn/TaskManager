ed5f478b5fb562eaae1a0801a3a7396428aef664
claudiogsn <claudiogsn2@gmail.com>
2023-04-17 15:53:08 -0300

    Push Semifinalista

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import { Routes, Route, useLocation } from 'react-router-dom'
-import Calendar from './pages/Calendar'
-import Profile from './pages/Profile'
+import { Routes, Route } from 'react-router-dom'
 import FormElements from './pages/Form/FormElements'
 import FormLayout from './pages/Form/FormLayout'
 import Tables from './pages/Tables'
-import Settings from './pages/Settings'
-import Chart from './pages/Chart'
-import Alerts from './pages/UiElements/Alerts'
-import Buttons from './pages/UiElements/Buttons'
-import SignIn from './pages/Authentication/SignIn'
-import SignUp from './pages/Authentication/SignUp'
 
 const App = () => {
   const [loading, setLoading] = useState(true)
@@ -33,17 +25,9 @@ const App = () => {
       <>
         <Routes>
           <Route exact path='/' element={<Tables />} />
-          <Route path='/calendar' element={<Calendar />} />
-          <Route path='/profile' element={<Profile />} />
           <Route path='/forms/form-elements' element={<FormElements />} />
           <Route path='/forms/form-layout/:id' element={<FormLayout />} />
           <Route path='/tables' element={<Tables />} />
-          <Route path='/settings' element={<Settings />} />
-          <Route path='/chart' element={<Chart />} />
-          <Route path='/ui/alerts' element={<Alerts />} />
-          <Route path='/ui/buttons' element={<Buttons />} />
-          <Route path='/auth/signin' element={<SignIn />} />
-          <Route path='/auth/signup' element={<SignUp />} />
         </Routes>
       </>
     )
